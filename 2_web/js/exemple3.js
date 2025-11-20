432fb14fc567767c107ab84c817b9476ccd4e511
naozzone <pc05imk@eduvaud.ch>
2025-11-20 15:17:41 +0100

    ajout de page web

--- 2_web/js/exemple3.js
+++ 2_web/js/exemple3.js
@@ -9,19 +9,37 @@ ctx_statut.fillRect(0,0,300,300);
 const dessin = document.getElementById("dessin");
 const context = dessin.getContext("2d");
 
+context.beginPath();
+context.strokeStyle = "red";
+context.moveTo(250, 100); //comme avec forward sur python
+context.lineTo(100,250);
+context.lineTo(250,400);
+context.lineTo(400,250);
+context.closePath(); //pour rejoindre les deux bouts
+context.fillStyle = "red";
+context.fill(); //remplir la forme
+context.stroke(); //fonction pour dessiner
+
+context.beginPath();
+context.arc(250, 250, 50, 0, 2 * Math.PI);  //position, position, rayon, angle de debart, angle d'arrivé
 context.fillStyle = "black";
-context.fillRect(0,0,200,100);
-context.fillRect(200,200,100,100);
+context.fill();
 
-for (var i = 0; i<5; i++) {
-    for (var j = 0; j<5; j++) {
-        if ((i + j) % 2 == 0){
-            context.fillStyle= "black";
 
-        } else {
-            context.fillStyle = "white"
-        }
 
-        context.fillRect (100 * i, 100 * j, 100, 100 );
-    }
-}
+// context.fillStyle = "black";
+// context.fillRect(0,0,200,100);
+// context.fillRect(200,200,100,100);
+
+// for (var i = 0; i<5; i++) {
+//     for (var j = 0; j<5; j++) {
+//         if ((i + j) % 2 == 0){
+//             context.fillStyle= "black";
+
+//         } else {
+//             context.fillStyle = "white"
+//         }
+
+//         context.fillRect (100 * i, 100 * j, 100, 100 );
+//     }
+// }
