d729649bff0285d198e4a289ad5cbd9d03df456f
naozzone <pc05imk@eduvaud.ch>
2025-12-07 22:05:11 +0100

    MODIF

--- 2_web/js/index.js
+++ 2_web/js/index.js
@@ -1,13 +1,13 @@
 const canvas_image = document.getElementById ("image");
 const ctx_image = canvas_image.getContext("2d");
 
-const img_airplane = new Image();
+const img_tiger = new Image();
 
 
-// img_airplane.onload = function() {
-//     ctx_image.drawImage(img_airplane, 150, 150, 50, 50);
+ //img_tiger.onload = function() {
+  //   ctx_image.drawImage(img_tiger, 0, 0, 50, 50);
 // }
-// img_airplane.src ="/2_web/image/airplane.svg";
+ //img_tiger.src ="/2_web/image/tiger.PNG";
 
 
 const canavasSizeX = 500;
@@ -20,7 +20,7 @@ var y = 1;
 
 
 function init() {
-    img_airplane.src ="/2_web/image/airplane.svg"
+    img_tiger.src ="/2_web/image/tiger.PNG"
     window.requestAnimationFrame(draw);
 }
 
@@ -29,7 +29,7 @@ function draw() {
     ctx_image.clearRect(0,0,500,500);
 
     if (x > canavasSizeX) {
-        ctx_image.translate(-canavasSizeX, -canavasSizeY);
+      ctx_image.translate(-canavasSizeX, -canavasSizeY);
         x = 1;
         y = 1;
     } else {
