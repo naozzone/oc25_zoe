4e965a8562d6cf83268d55c7dfe35c7a0ebbd215
naozzone <pc05imk@eduvaud.ch>
2026-01-15 15:20:31 +0100

    jeu2d

---
+++ 4_jeu3d/tutoriel2d/main.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@export var mob_scene: PackedScene
+var score
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+func _on_mob_timer_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_score_timer_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_start_timer_timeout() -> void:
+	pass # Replace with function body.
